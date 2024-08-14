4525e2bbdf3e09ad5c0c091b0b5b7c68757a57c9
andylockran <andy@zrmt.com>
2024-08-14 21:14:40 +0100

    Fixed pipeline permissions

--- cdk-pipeline/lib/cdk-pipeline-stack.ts
+++ cdk-pipeline/lib/cdk-pipeline-stack.ts
@@ -56,7 +56,7 @@ export class CdkPipelineStack extends cdk.Stack {
               new PolicyStatement({
                 effect: Effect.ALLOW,
                 actions: [
-                  "s3:ListObject*",
+                  "s3:List*",
                   "s3:Put*"
                 ],
                 resources: [
