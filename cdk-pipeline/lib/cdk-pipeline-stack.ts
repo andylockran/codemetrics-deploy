414b2c1bffd9e08bc1a101b0acb87909d47c36d9
andylockran <andy@zrmt.com>
2024-08-14 21:31:44 +0100

    Fixed package-lock.json

--- cdk-pipeline/lib/cdk-pipeline-stack.ts
+++ cdk-pipeline/lib/cdk-pipeline-stack.ts
@@ -78,7 +78,7 @@ export class CdkPipelineStack extends cdk.Stack {
         input: CodePipelineSource.s3(deploymentBucket, 'codemetrics-deploy.zip', {
           trigger: S3Trigger.EVENTS
         }),
-        commands: ["ls -ltrh", "cd codemetrics-deploy","npm ci", "npm run build", "npx cdk synth"],
+        commands: ["ls -ltrhR", "cd codemetrics-deploy","npm ci", "npm run build", "npx cdk synth"],
       }),
     });
   }
